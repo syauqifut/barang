3899d4847763f9b689e5f81da7b2c341b5ed7cdd
syauqifut <68411459+syauqifut@users.noreply.github.com>
2021-11-20 17:54:40 +0700

    remove updatedby createdby barang table

---
+++ database/migrations/2021_11_20_105216_remove_column_barang_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveColumnBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            //
+        });
+    }
+}
